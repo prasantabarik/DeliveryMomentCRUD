3d6c3a6e6f60100c59107aabd8316e782ae9cbea
Pitchu <pitchunath.nj@tcs.com>
2020-12-08 14:12:14 +0530

    reading dapr secrets

--- src/main/kotlin/com/tcs/service/ServiceKotlinTemplateApplication.kt
+++ src/main/kotlin/com/tcs/service/ServiceKotlinTemplateApplication.kt
@@ -2,15 +2,26 @@ package com.tcs.service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import khttp.get
+import khttp.responses.Response
+import org.json.JSONObject
+import org.springframework.boot.SpringApplication
 
 @SpringBootApplication
 class ServiceKotlinTemplateApplication
 
 fun main(args: Array<String>) {
-    runApplication<ServiceKotlinTemplateApplication>(*args)
-    //get(url = "http://localhost:3500/v1.0/secrets/azurekeyvault/deliverymomentdbapi")
-    println(get(url = "http://localhost:3500/v1.0/secrets/azurekeyvault/deliverymomentdbapi?metadata.namespace=edppublic-deliverymomentcrud-dev"))
-    println(get(url = "http://localhost:3500/v1.0/secrets/azurekeyvault/deliverycruddb?metadata.namespace=edppublic-deliverymomentcrud-dev"))
-    
+
+    println("this is how dapr works")
+
+    val response1 : Response = khttp.get("http://localhost:3500/v1.0/secrets/azurekeyvault/deliverymomentdbapi")
+    val obj1 : JSONObject = response1.jsonObject
+
+    val response2 : Response = khttp.get("http://localhost:3500/v1.0/secrets/azurekeyvault/deliverycruddb")
+    val obj2 : JSONObject = response2.jsonObject
+
+    var app:SpringApplication = SpringApplication(ServiceKotlinTemplateApplication::class.java)
+
+    app.run(String.format("--server.port=%d", 8080),
+            String.format("--spring.data.mongodb.database=%s", obj2["deliverycruddb"].toString()),
+            String.format("--spring.data.mongodb.Uri=%s", obj1["deliverymomentdbapi"].toString()))
 }
